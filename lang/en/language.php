bdd67d9f67ac9922520aad31eda5481a6c21a9e6
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 01:19:15 +0200

    refactor

--- lang/en/language.php
+++ lang/en/language.php
@@ -4,22 +4,15 @@ $LANG = array();
 
 // Translation of file modules/video/module.php
 
-$LANG['video_add_file'] = 'video_add_file';
-$LANG['video_delete_file'] = 'video_delete_file';
-$LANG['video_edit_file'] = 'video_edit_file';
-$LANG['file_delete_label'] = 'file_delete_label';
+$LANG['Add video'] = 'Add video';
+$LANG['Delete video'] = 'Delete video';
+$LANG['Edit video'] = 'Edit video';
 $LANG['Delete'] = 'Delete';
-$LANG['file_edit_legend'] = 'file_edit_legend';
-$LANG['system_submit_update'] = 'system_submit_update';
-$LANG['file_add_legend'] = 'file_add_legend';
-$LANG['system_submit_add'] = 'system_submit_add';
-$LANG['file_abstract_label'] = 'file_abstract_label';
-$LANG['video_could_not_transform_to_flv'] = 'video_could_not_transform_to_flv';
+$LANG['Update video'] = 'Update video';
+$LANG['Abstract'] = 'Abstract';
+$LANG['Could not transform to flv'] = 'Could not transform to flv';
 $LANG['video_no_file_specified'] = 'video_no_file_specified';
-$LANG['video_add'] = 'video_add';
-$LANG['video_download'] = 'video_download';
-$LANG['video_edit'] = 'video_edit';
-$LANG['video_delete'] = 'video_delete';
-$LANG['video_file_added'] = 'video_file_added';
-$LANG['video_file_deleted'] = 'video_file_deleted';
-$LANG['video_file_edited'] = 'video_file_edited';
+$LANG['Download video'] = 'Download video';
+$LANG['Video was added'] = 'Video was added';
+$LANG['Video was deleted'] = 'Video was deleted';
+$LANG['Video was edited'] = 'Video was edited';
