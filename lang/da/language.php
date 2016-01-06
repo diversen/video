32240f4051a2c1b5f80f3c22d763c272fe3b93f5
Dennis Iversen <dennis.iversen@gmail.com>
2016-01-06 12:16:46 +0100

    lang da

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,20 +2,20 @@
 
 $LANG = array();
 
-$LANG['Add video'] = 'Tilføj videoer';
-$LANG['Edit video'] = 'Redigere video';
+$LANG['Add video'] = 'Tilføj video';
+$LANG['Edit video'] = 'Rediger video';
 $LANG['Delete video'] = 'Slet video';
 $LANG['Go back'] = 'Gå tilbage';
 $LANG['Uploaded videos'] = 'Uploadede videoer';
-$LANG['Videos under progress'] = 'Videoer under udvikling';
+$LANG['Videos under progress'] = 'Videoer som i øjeblikket bliver transformeret';
 $LANG['Update video'] = 'Opdater video';
 $LANG['Title'] = 'Titel';
-$LANG['Content-type is not allowed'] = 'Indhold-typen er ikke tilladt';
-$LANG['No files were selected'] = 'Ingen filer blev udvalgt';
-$LANG['Videos'] = 'Videos';
-$LANG['Download video'] = 'Download video';
-$LANG['Edit'] = 'Redigere';
+$LANG['Content-type is not allowed'] = 'Indholds-typen er ikke tilladt';
+$LANG['No files were selected'] = 'Ingen filer blev uploadet';
+$LANG['Videos'] = 'Videoer';
+$LANG['Download video'] = 'Vis video';
+$LANG['Edit'] = 'Rediger';
 $LANG['Delete'] = 'Slet';
-$LANG['Video(s) uploaded. They are now being transformed. You may move away, and return to see the progress'] = 'Video (er) uploadet. De er nu ved at blive forvandlet. Du må bevæge sig væk, og vende tilbage for at se de fremskridt,';
+$LANG['Video(s) uploaded. They are now being transformed. You may move away, and return to see the progress'] = 'Video(er) blev uploadet. De er nu ved at blive transformeret. Du må gerne bevæge dig væk fra denne side. Du kan altid vende tilbage til denne side og se fremgang.';
 $LANG['Video was deleted'] = 'Video blev slettet';
 $LANG['Video was edited'] = 'Video blev redigeret';
